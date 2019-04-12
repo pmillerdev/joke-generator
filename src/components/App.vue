f6954f65b49765af0aad165472bda06da97d391e
Pete <pmillerdev@gmail.com>
2019-04-12 17:03:03 +0100

    feat: create joke component with basic styles

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,16 +2,29 @@
     <div id="app">
         <h3>Joke Generator</h3>
         <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
-        <div>{{$store.state}}</div>
+        <br />
+        <br />
+        <div class="col-sm-12">
+            <Joke
+                v-for="(joke, index) in $store.state.jokes"
+                v-bind:joke="joke"
+                v-bind:key="index"
+            />
+        </div>
+        <div class="">{{$store.state}}</div>
     </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
     methods: mapActions([
         'initJokes'
-    ])
+    ]),
+    components: {
+        Joke
+    }
 }
 </script>
